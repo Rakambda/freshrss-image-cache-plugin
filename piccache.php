f59f9de9bd8499b3b6a6f5d31a82f16aa4748b45
Thomas Couchoud <zerderr@gmail.com>
2023-10-11 17:44:41 +0200

    Display errors

--- piccache.php
+++ piccache.php
@@ -30,7 +30,7 @@ ini_set("log_errors", 1);
 ini_set("log_errors_max_len", 2048);
 ini_set("error_log", $logFile);
 error_reporting(E_ALL);
-ini_set('display_errors', '0');
+ini_set('display_errors', '1');
 
 class Config
 {
