5cc89ae0f273f8f26af1124410b658a3595698b9
Thomas Couchoud <1688389+Rakambda@users.noreply.github.com>
2024-10-29 19:04:48 +0100

    Important css class

--- extension.php
+++ extension.php
@@ -42,7 +42,7 @@ class ImageCacheExtension extends Minz_Extension
 img.cache-image, video.cache-image {
     min-width: 100px;
     min-height: 100px;
-    max-height: 50vh;
+    max-height: 50vh !important;
     object-fit: contain;
     background-color: red;
 }
